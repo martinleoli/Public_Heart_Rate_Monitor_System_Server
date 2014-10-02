52af165b12e3c599273441a90ba8432ff1dad76f
Bing Li <martinleo.li@gmail.com>
2014-10-02 17:02:43 -0400

    add methods into monitor.Interfaces

--- src/monitor/Interfaces.java
+++ src/monitor/Interfaces.java
@@ -1,5 +1,21 @@
 package monitor;
 
+import database.interfaces.PersonStatus;
+
 public class Interfaces {
 
+	/**
+	 * create an new windows
+	 */
+	public static void startup() {
+	}
+
+	/**
+	 * draw all user on the map
+	 * 
+	 * @param list
+	 *            a list contains all person status
+	 */
+	public static void drawUsers(Iterable<PersonStatus> list) {
+	}
 }
