3a456caf1438c73ad15c76320c0182a70d9a7496
Bing Li <martinleo.li@gmail.com>
2014-10-02 12:14:29 -0400

    create database.interfaces.Person

---
+++ src/database/interfaces/Person.java
@@ -0,0 +1,56 @@
+package database.interfaces;
+
+
+/**
+ * this interface contains all basic user information
+ * @author Bing Li
+ *
+ */
+public interface Person {
+
+	/**
+	 * set user ID
+	 * 
+	 * @param n
+	 *            a ten digit number
+	 */
+	public abstract void setID(long n);
+
+	/**
+	 * retrieve user ID
+	 * 
+	 * @return user ID
+	 */
+	public abstract long getID();
+
+	/**
+	 * set user's first name
+	 * 
+	 * @param n
+	 *            a string contain user's first name
+	 */
+	public abstract void setFirstName(String n);
+
+	/**
+	 * set usr's last name
+	 * 
+	 * @param n
+	 *            a string contain user's last name
+	 */
+	public abstract void setLastName(String n);
+
+	/**
+	 * retrieve user's first name
+	 * 
+	 * @return a String Object contains user's first name
+	 */
+	public abstract String getFirstName();
+
+	/**
+	 * retrieve user's last name
+	 * 
+	 * @return a String object contains user's last name
+	 */
+	public abstract String getLastName();
+
+}
