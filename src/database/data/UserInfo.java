47b49825e69af59d265805ccabc648d35beb4e37
Bing Li <martinleo.li@gmail.com>
2014-10-03 12:24:24 -0400

    create database.data.UserInfo

---
+++ src/database/data/UserInfo.java
@@ -0,0 +1,48 @@
+package database.data;
+
+import database.interfaces.Person;
+
+public class UserInfo implements Person {
+
+	@Override
+	public void setID(long ID) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setFirstName(String n) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setLastName(String n) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLastName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
