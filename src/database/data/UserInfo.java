87afbafb6c977d55d9a8a19ad2fce76f3fafeb96
Bing Li <martinleo.li@gmail.com>
2014-10-03 12:41:09 -0400

    database.data.UserInfo v1

--- src/database/data/UserInfo.java
+++ src/database/data/UserInfo.java
@@ -2,46 +2,100 @@ package database.data;
 
 import database.interfaces.Person;
 
+/**
+ * Contains all user information
+ * 
+ * @author Bing Li
+ *
+ */
 public class UserInfo implements Person {
 
-	@Override
+	private final long ID;
+	private String firstName = "";
+	private String lastName = "";
+
+	/**
+	 * normal constructor
+	 * 
+	 * @param ID
+	 *            a ten digit number presents user ID
+	 * @param firstName
+	 *            a String object presents user's first name
+	 * @param lastName
+	 *            a String object presents user's last name
+	 */
+	public UserInfo(long ID, String firstName, String lastName) {
+		this(ID);
+		this.firstName = firstName;
+		this.lastName = lastName;
+
+	}
+
+	/**
+	 * constructor with empty user name
+	 * 
+	 * @param ID
+	 *            a ten digit number presents user ID
+	 */
+	public UserInfo(long ID) {
+		this.ID = ID;
+	}
+
+	/**
+	 * test constructor, user's ID will be -1
+	 */
+	public UserInfo() {
+		this(-1);
+	}
+
+	/**
+	 * this method is useless
+	 */
 	public void setID(long ID) {
-		// TODO Auto-generated method stub
+
+		System.out
+				.println("Don't use database.data.UserInfo.setID(), because ID is final variable!");
 
 	}
 
 	@Override
 	public long getID() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return this.ID;
 	}
 
 	@Override
 	public void setFirstName(String n) {
-		// TODO Auto-generated method stub
+		this.firstName = n;
 
 	}
 
 	@Override
 	public void setLastName(String n) {
-		// TODO Auto-generated method stub
+		this.lastName = n;
 
 	}
 
 	@Override
 	public String getFirstName() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.firstName;
 	}
 
 	@Override
 	public String getLastName() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.lastName;
+	}
+
+	@Override
+	public String toString() {
+		return this.ID + " " + this.firstName + " " + this.lastName;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		UserInfo u = new UserInfo(1000,"asdf","zxcv");
+		System.out.println(u);
 
 	}
 
