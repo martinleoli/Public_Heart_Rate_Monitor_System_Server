3304f9493a4ee4b1c92817485813138fb3e5f560
Bing Li <martinleo.li@gmail.com>
2014-10-08 13:45:05 -0400

    finish UI test.client.ClientGUI

--- src/test/client/ClientGUI.java
+++ src/test/client/ClientGUI.java
@@ -12,8 +12,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.SwingConstants;
-
+import server.ServerInfo;
 import database.data.Location;
 import database.data.UserInfo;
 import database.data.UserStatus;
@@ -55,11 +54,18 @@ public class ClientGUI extends JFrame {
 		JTextField userID = new JTextField(us.getPerson().getID() + "");
 		JTextField userFirstName = new JTextField(us.getPerson().getFirstName());
 		JTextField userLastName = new JTextField(us.getPerson().getLastName());
-		JTextField longtitude = new JTextField(us.getCoordinate().getLongitude()+"");
-		JTextField latitude = new JTextField(us.getCoordinate().getLatitude()+"");
+		JTextField longtitude = new JTextField(us.getCoordinate()
+				.getLongitude() + "");
+		JTextField latitude = new JTextField(us.getCoordinate().getLatitude()
+				+ "");
+		JTextField heartRate = new JTextField(us.getHeartRate()+"");
+		JTextField IP = new JTextField("localhost");
+		JTextField port = new JTextField(ServerInfo.server_port_number+"");
+		
 		JScrollPane logPane = new JScrollPane(log,
 				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
 		JLabel label1 = new JLabel("User Information");
 		JLabel label2 = new JLabel("User ID:");
 		JLabel label3 = new JLabel("First Name:");
@@ -67,6 +73,11 @@ public class ClientGUI extends JFrame {
 		JLabel label5 = new JLabel("Coordinate");
 		JLabel label6 = new JLabel("Longtitude:");
 		JLabel label7 = new JLabel("Latitude:");
+		JLabel label8 = new JLabel("Heart Rate:");
+		JLabel label9 = new JLabel(
+				"----------------------------------------------------------------------------------------------");
+		JLabel label10 = new JLabel("Server IP:");
+		JLabel label11 = new JLabel("Port:");
 		// ----------------------Edit here-------------------
 		label1.setBounds(10, 10, 150, 20);
 		label2.setBounds(10, 35, 70, 20);
@@ -75,12 +86,25 @@ public class ClientGUI extends JFrame {
 		label5.setBounds(10, 85, 90, 20);
 		label6.setBounds(10, 110, 90, 20);
 		label7.setBounds(210, 110, 90, 20);
+		label8.setBounds(10, 135, 90, 20);
+		label9.setBounds(10, 160, 480, 10);
+		label10.setBounds(10, 185, 100, 20);
+		label11.setBounds(10,210,100,20);
 
 		userID.setBounds(80, 35, 200, 20);
 		userFirstName.setBounds(100, 60, 100, 20);
 		userLastName.setBounds(310, 60, 100, 20);
 		longtitude.setBounds(100, 110, 100, 20);
 		latitude.setBounds(310, 110, 100, 20);
+		heartRate.setBounds(100, 135, 100, 20);
+		IP.setBounds(120, 185, 300, 20);
+		port.setBounds(120, 210, 100, 20);
+
+		setButton.setBounds(350, 135, 100, 20);
+		connectButton.setBounds(300, 210, 150, 20);
+		
+		logPane.setBounds(10, 235, 480, 250);
+		
 
 		log.setEditable(false);
 
@@ -99,6 +123,16 @@ public class ClientGUI extends JFrame {
 		window.add(longtitude);
 		window.add(label7);
 		window.add(latitude);
+		window.add(label8);
+		window.add(heartRate);
+		window.add(setButton);
+		window.add(label9);
+		window.add(label10);
+		window.add(IP);
+		window.add(label11);
+		window.add(port);
+		window.add(connectButton);
+		window.add(logPane);
 		// ----------------------Edit here-------------------
 
 		// set windows size and postion
