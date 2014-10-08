09a4d9a15f0ee09e5eda51e8da2ced40791f8a89
Bing Li <martinleo.li@gmail.com>
2014-10-08 11:40:04 -0400

    add labels and textfield into test.client.ClientGUI

--- src/test/client/ClientGUI.java
+++ src/test/client/ClientGUI.java
@@ -10,6 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 
@@ -51,14 +52,35 @@ public class ClientGUI extends JFrame {
 		JButton setButton = new JButton("Set");
 		JButton connectButton = new JButton("Connect");
 		JTextArea log = new JTextArea();
+		JTextField userID = new JTextField(us.getPerson().getID() + "");
+		JTextField userFirstName = new JTextField(us.getPerson().getFirstName());
+		JTextField userLastName = new JTextField(us.getPerson().getLastName());
+		JTextField longtitude = new JTextField(us.getCoordinate().getLongitude()+"");
+		JTextField latitude = new JTextField(us.getCoordinate().getLatitude()+"");
 		JScrollPane logPane = new JScrollPane(log,
 				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		JLabel label1 = new JLabel("User Information");
 		JLabel label2 = new JLabel("User ID:");
+		JLabel label3 = new JLabel("First Name:");
+		JLabel label4 = new JLabel("Last Name:");
+		JLabel label5 = new JLabel("Coordinate");
+		JLabel label6 = new JLabel("Longtitude:");
+		JLabel label7 = new JLabel("Latitude:");
 		// ----------------------Edit here-------------------
 		label1.setBounds(10, 10, 150, 20);
-		label2.setBounds(10, 30, 100, 20);
+		label2.setBounds(10, 35, 70, 20);
+		label3.setBounds(10, 60, 90, 20);
+		label4.setBounds(210, 60, 90, 20);
+		label5.setBounds(10, 85, 90, 20);
+		label6.setBounds(10, 110, 90, 20);
+		label7.setBounds(210, 110, 90, 20);
+
+		userID.setBounds(80, 35, 200, 20);
+		userFirstName.setBounds(100, 60, 100, 20);
+		userLastName.setBounds(310, 60, 100, 20);
+		longtitude.setBounds(100, 110, 100, 20);
+		latitude.setBounds(310, 110, 100, 20);
 
 		log.setEditable(false);
 
@@ -67,6 +89,16 @@ public class ClientGUI extends JFrame {
 		window.setLayout(null);
 		window.add(label1);
 		window.add(label2);
+		window.add(userID);
+		window.add(label3);
+		window.add(userFirstName);
+		window.add(label4);
+		window.add(userLastName);
+		window.add(label5);
+		window.add(label6);
+		window.add(longtitude);
+		window.add(label7);
+		window.add(latitude);
 		// ----------------------Edit here-------------------
 
 		// set windows size and postion
