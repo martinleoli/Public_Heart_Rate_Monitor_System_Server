dee92956bfed7375854da92b6efbcf15c0ba196b
Bing Li <martinleo.li@gmail.com>
2014-10-12 23:00:55 -0700

    add entry into test.client.Main

--- src/test/client/Main.java
+++ src/test/client/Main.java
@@ -1,5 +1,13 @@
 package test.client;
 
+/**
+ * This is the Main class of client
+ * 
+ * @author Bing Li
+ *
+ */
 public class Main {
-
+	public static void main(String args[]) {
+		ClientGUI client = new ClientGUI();
+	}
 }
